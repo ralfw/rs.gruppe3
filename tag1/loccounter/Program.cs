b3f663a63268c7b78d578e275429c381a58ad64c
Ralf Westphal <info@ralfw.de>
2014-09-23 09:30:49 +0200

    finished

--- tag1/loccounter/Program.cs
+++ tag1/loccounter/Program.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
+// ein kommentar
 namespace loccounter
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
+			// noch ein kommentar
 			var filenames = Directory.GetFiles (args[0], "*.cs", SearchOption.AllDirectories);
 
 			var totalLOC = 0;
 			foreach (var fn in filenames) {
 				var lines = File.ReadAllLines (fn);
+				lines = lines.Where (l => !l.StartsWith ("//")).ToArray();
+				lines = lines.Where (l => l.Trim () != "").ToArray ();
 				totalLOC += lines.Length;
 			}
 
