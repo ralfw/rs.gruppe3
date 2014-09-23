c90e5864b2e0a7a6afb3d8675e7a742fae360b82
Ralf Westphal <info@ralfw.de>
2014-09-23 14:13:07 +0200

    umbau auf reine integration in Main()

--- tag1/loccounter/Program.cs
+++ tag1/loccounter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 // ein kommentar
 namespace loccounter
@@ -9,18 +10,42 @@ namespace loccounter
 	{
 		public static void Main (string[] args)
 		{
-			// noch ein kommentar
-			var filenames = Directory.GetFiles (args[0], "*.cs", SearchOption.AllDirectories);
+			var quelle = Verzeichnis_bestimmen (args);
+			var dateinamen = Dateien_sammeln (quelle);
+			var dateien = Relevante_Dateien_erkennen (dateinamen);
+			var zeilen = Relevante_Zeilen_extrahieren (dateien);
+			var loc = Ergebnis_bestimmen (zeilen);
+			Ergebnis_präsentieren (loc);
+		}
 
-			var totalLOC = 0;
-			foreach (var fn in filenames) {
-				var lines = File.ReadAllLines (fn);
-				lines = lines.Where (l => !l.StartsWith ("//")).ToArray();
-				lines = lines.Where (l => l.Trim () != "").ToArray ();
-				totalLOC += lines.Length;
-			}
+		static string Verzeichnis_bestimmen (string[] args)
+		{
+			return args [0];
+		}
 
-			Console.WriteLine (totalLOC);
+		static string[] Dateien_sammeln (string quelle)
+		{
+			return Directory.GetFiles (quelle, "*.*", SearchOption.AllDirectories);
+		}
+
+		static IEnumerable<string[]>  Relevante_Dateien_erkennen (string[] dateinamen)
+		{
+			return dateinamen.Select (dn => File.ReadAllLines (dn));
+		}
+
+		static IEnumerable<string> Relevante_Zeilen_extrahieren (IEnumerable<string[]> dateien)
+		{
+			return dateien.SelectMany (d => d.Where (l => l.Trim () != "").Where (l => !l.Trim ().StartsWith ("//")));
+		}
+
+		static object Ergebnis_bestimmen (IEnumerable<string> zeilen)
+		{
+			return zeilen.Count ();
+		}
+
+		static void Ergebnis_präsentieren (object loc)
+		{
+			Console.Write (loc);
 		}
 	}
 }
