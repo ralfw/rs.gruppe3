851462b9fc7a8d3be0da1ad754adf214e3b877ef
Ralf Westphal <info@ralfw.de>
2014-10-23 19:25:10 +0200

    akronyme werden gefiltert

--- tag2u3/spellchecker/Spellchecker.cs
+++ tag2u3/spellchecker/Spellchecker.cs
@@ -7,14 +7,19 @@ namespace spellchecker
 {
 	class Spellchecker {
 		public string[] In_Worte_zerlegen(string text) {
-			return text.Split (new[]{ ' ', '.', ',', '!', ';', ':', '?' }, StringSplitOptions.RemoveEmptyEntries);
+			return text.Split (new[]{ ' ', '.', ',', '!', ';', ':', '?', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 		}
 
 		public string[] Worte_prüfen(Dictionary dict, string[] worte) {
-			return worte.Select (w => w.ToLower ())
+			return worte.Where(w => !Is_an_acronym(w))
+						.Select (w => w.ToLower ())
 				        .Where (w => !dict.Contains (w))
 				        .Distinct ()
 				        .ToArray ();
+		}
+
+		private bool Is_an_acronym(string word) {
+			return string.Compare (word, word.ToUpper (), false) == 0;
 		}
 	}
 }
