3ca345ab5fc49bc19d2bc51cc4982768554a15bc
Ralf Westphal <info@ralfw.de>
2015-01-29 17:13:35 +0100

    Trivial symbol generator (l=1..25)

--- tag4u5/nback/nback.body/SymbolGenerator.cs
+++ tag4u5/nback/nback.body/SymbolGenerator.cs
@@ -5,5 +5,18 @@ namespace nback.body
 {
 	class SymbolGenerator
 	{
+		private int n, l;
+
+		// Added l to the param list
+		public char Start_symbol_series(int n, int l) {
+			this.l = l;
+			this.n = n;
+			return 'A' + this.l--;
+		}
+			
+		// Added a second function to generate the next symbol
+		public char Get_next_symbol_in_series() {
+			return 'A' + this.l--;
+		}
 	}
 }
